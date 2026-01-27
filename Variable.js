cce2546c6b1504e65d5c5d8109d353c2c06796ed
Nikhil Kholiya <nikhilkholiya08@gmail.com>
2026-01-27 18:22:17 +0530

    javascript variable

---
+++ Variable.js
@@ -0,0 +1,15 @@
+console.log('Javascript Variables');
+console.log(`-Variables in JavaScript
+In JavaScript, variables are used to store data values. You can declare variables using three keywords: var, let, and const.`)
+console.log(`1.var: Declares a variable with function scope.`);
+console.log(`Example:`);
+var x = 10;
+console.log('Var output', x);;
+console.log(`2.let: Declares a block-scoped variable.`);
+console.log(`Example:`);
+let y = 20;
+console.log('Let output', y);;
+console.log(`3.const: Declares a block-scoped constant variable. Its value cannot be changed once assigned.`);
+console.log(`Example:`);
+const z = 30;
+console.log('Const output', z);;
