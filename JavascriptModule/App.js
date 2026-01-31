45b7aa81fba06c2da684f93159e6aa19d98892b1
Nikhil Kholiya <nikhilkholiya08@gmail.com>
2026-01-31 18:22:42 +0530

    modules in javascript

---
+++ JavascriptModule/App.js
@@ -0,0 +1,9 @@
+import add, { sub } from "./Math.js";
+console.log('Javascript Module');
+console.log('Export from the Math.js File and import in App.js file');
+//add two number
+console.log('Adding two number using Export module');
+console.log(add(3, 3));
+//subtract two number
+console.log('Subtract two number usinf export module');
+console.log(sub(3,3))
---
+++ JavascriptModule/Math.js
@@ -0,0 +1,8 @@
+const add = (x, y) => {
+    return x + y;
+}
+export default add;
+
+export const sub = (x,y) => {
+    return x-y
+}
