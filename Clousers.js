658e022dff9a6252009543d713baafa3ccd74128
Nikhil Kholiya <nikhilkholiya08@gmail.com>
2026-02-02 17:37:56 +0530

    clousers in javascript

--- Clousers.js
+++ Clousers.js
@@ -1,1 +1,17 @@
 console.log('Clousers in Javascript');
+console.log(`Clouser is a function that remember and can access variable from its outer scope even after that outer function has executin.`);
+
+console.log('Clouser Example:The Private Counter:');;
+
+function createCounter() {
+    let count = 0;
+    return function () {
+        count++;
+        return count;
+    }
+}
+
+const myCounter = createCounter();
+console.log(myCounter())
+console.log(myCounter())
+console.log(myCounter())
