d670f8965eb434827eed9b0136ec2457614edd17
Nikhil Kholiya <nikhilkholiya08@gmail.com>
2026-02-06 17:57:46 +0530

    todo list complete

--- JavascriptProjects/TodoList/App.js
+++ JavascriptProjects/TodoList/App.js
@@ -1,2 +1,24 @@
 console.log('TodoList App');
-alert('TodoList App')
+
+function SaveTodo() {
+    let taskInput = document.getElementById('textInput');
+    let taskInputValue = taskInput.value.trim();
+    let taskLists = document.getElementById('taskList');
+    let taskList = document.createElement('li');
+    taskList.textContent = taskInputValue;
+    if (taskInput.value === '') {
+        alert('Enter todo list');
+        return
+    }
+    taskLists.appendChild(taskList);
+    let deleteBtn = document.createElement('button');
+    deleteBtn.textContent = 'Delete Todo';
+    deleteBtn.addEventListener('click', () => {
+        taskLists.removeChild(taskList);
+        taskLists.removeChild(deleteBtn);
+    })
+    taskLists.appendChild(deleteBtn);
+    
+    
+    taskInput.value = '';
+}
